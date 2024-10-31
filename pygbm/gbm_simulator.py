1d306e80d667299c2cdb487efda0b13fa38397fe
Laura Just Fung <ljustfung@gmail.com>
2024-10-31 23:09:44 +0000

    documentation

--- pygbm/gbm_simulator.py
+++ pygbm/gbm_simulator.py
@@ -1,10 +1,38 @@
 import numpy as np
 class GBMSimulator:
+  """
+  A class to simulate geometric Brownian motion (GBM).
+
+  Attributes:
+    y0 (float): Initial value of the Brownian motion.
+    mu (float): Drift coefficient.
+    sigma (float): Diffusion coefficient.
+  """
   def __init__(self, y0, mu, sigma):
+    """
+    Initializes the GBMSimulator with given parameters.
+
+    Parameters:
+      y0 (float): Initial value of the Brownian motion.
+      mu (float): Drift coefficient (expected return).
+      sigma (float): Diffusion coefficient (standard deviation).
+    """
     self.y0 = y0
     self.mu = mu
     self.sigma = sigma
   def simulate_path(self, T, N):
+    """
+    Simulates a path of geometric Brownian motion.
+
+    Parameters:
+      T (float): Total time for the simulation.
+      N (int): Number of time steps.
+
+    Returns:
+      tuple: A tuple containing:
+          - t_values (numpy.ndarray): Array of time points.
+          - y_values (numpy.ndarray): Simulated values at each time point.
+    """
     dt = T / N
     t_values = np.linspace(0, T, N + 1)
     y_values = np.zeros(N + 1)
@@ -15,7 +43,24 @@ class GBMSimulator:
     return t_values, y_values
   
 class EulerMaruyama(GBMSimulator):
+  """
+  A class to simulate geometric Brownian motion using the Euler-Maruyama method.
+
+  Inherits from the GBMSimulator class.
+  """
   def simulate_empath(self, T, N):
+    """
+    Simulates a path of geometric Brownian motion using the Euler-Maruyama method.
+
+    Parameters:
+      T (float): Total time for the simulation.
+      N (int): Number of time steps.
+
+    Returns:
+      tuple: A tuple containing:
+          - t_values (numpy.ndarray): Array of time points.
+          - y_values (numpy.ndarray): Simulated values at each time point.
+    """
     dt = T/N
     t_values = np.linspace(0, T, N+1)
     y_values = np.zeros(N + 1)
@@ -27,7 +72,24 @@ class EulerMaruyama(GBMSimulator):
     return t_values, y_values
 
 class MilsteinSimulator(GBMSimulator):
+  """
+  A class to simulate geometric Brownian motion using the Milstein method.
+
+  Inherits from the GBMSimulator class.
+  """
   def simulate_milpath(self, T, N):
+    """
+    Simulates a path of geometric Brownian motion using the Milstein method.
+
+    Parameters:
+      T (float): Total time for the simulation.
+      N (int): Number of time steps.
+
+    Returns:
+      tuple: A tuple containing:
+          - t_values (numpy.ndarray): Array of time points.
+          - y_values (numpy.ndarray): Simulated values at each time point.
+    """
     dt = T/N
     t_values = np.linspace(0, T, N+1)
     y_values = np.zeros(N + 1)
