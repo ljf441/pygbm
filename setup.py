4187fa0d4df58c00c204dbf93c76bb9263bd2f0a
Laura Just Fung <18justfungl2@gmail.com>
2024-11-01 09:29:46 +0000

    Update workflow to publish to GitHub Packages

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygbm',
-    use_scm_version=True,  # Use setuptools_scm to manage versioning
-    setup_requires=['setuptools>=42', 'setuptools_scm'],  # Ensure setuptools_scm is available
+    version='0.1.dev1',  # Update to a version without local identifier
     packages=find_packages(),
     install_requires=[
         'numpy',
@@ -13,20 +12,19 @@ setup(
     entry_points={
         'console_scripts': [
             'pygbm=pygbm.cli:main',
-        ],
+        ]
     },
     description='A Python package for geometric Brownian motion simulation.',
-    long_description=open('README.md').read(),  # Read the long description from your README
-    long_description_content_type='text/markdown',  # Specify the content type of the long description
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Laura Just Fung',
     author_email='lj441@cam.ac.uk',
-    license='MIT',  # Specify your license
+    license='MIT',
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Physicists',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Physicists",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
     ],
-    python_requires='>=3.6',  # Specify the Python version requirement
 )
