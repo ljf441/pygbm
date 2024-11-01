7a51a6860aabda1b83a867ca71942036c1f63c87
Laura Just Fung <18justfungl2@gmail.com>
2024-11-01 09:47:59 +0000

    Update workflow

--- setup.py
+++ setup.py
@@ -2,29 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygbm',
-    version='0.1.dev1',  # Update to a version without local identifier
+    version='0.1.0',  # Update the version here
     packages=find_packages(),
     install_requires=[
         'numpy',
         'matplotlib',
-        'jupyter',
+        'jupyter'
     ],
     entry_points={
         'console_scripts': [
             'pygbm=pygbm.cli:main',
         ]
     },
-    description='A Python package for geometric Brownian motion simulation.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Laura Just Fung',
-    author_email='lj441@cam.ac.uk',
-    license='MIT',
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Physicists",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries",
-    ],
 )
