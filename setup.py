a5d7f027a6154e1841a2eb1a9bb3762d1a899063
ljf441 <18justfungl2@gmail.com>
2024-11-05 11:53:27 +0000

    pypi upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pygbm',
+    name='ljf441-pygbm',
     version='0.1.0',  # Update the version here
     packages=find_packages(),
     install_requires=[
